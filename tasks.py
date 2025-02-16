9a7122ff76bd8df097c60b25b2a77da59b3edb7a
megbroc7 <34313047+megbroc7@users.noreply.github.com>
2025-02-16 15:35:33 +0100

    Putting secrets behind env in DO

--- tasks.py
+++ tasks.py
@@ -1,11 +1,12 @@
+import os
 from celery import Celery
 
-# Configure Celery with Redis as the broker and backend
-celery = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+redis_url = os.environ.get("REDIS_URL")
+celery = Celery("tasks", broker=redis_url, backend=redis_url)
 
 @celery.task
 def process_csv_task(upload_path, output_path):
     # Import process_file here to avoid circular imports
     from app import process_file
     process_file(upload_path, output_path)
-    return "Processing complete"
+    return "Processing is complete"
